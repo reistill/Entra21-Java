cebaf949265ae9583641fdc54f2e52f4a4b59d9e
reistill <joelreistill@gmail.com>
2025-04-22 11:20:04 -0300

    ex metodos att
    exercício 14 atualizado!

--- ExMetodos/ExMet014.java
+++ ExMetodos/ExMet014.java
@@ -8,8 +8,14 @@ import java.util.Scanner;
 
 public class ExMet014 {
 	
-	public static double mediaAritmetica(double nota1, double nota2, double nota3) {
-		return (nota1 + nota2 + nota3) / 3;
+	public static double mediaAritmetica(double vetor[]) {
+		
+		double soma = 0;
+		
+		for (int i = 0; i < vetor.length; i++) {
+			soma += vetor[i];
+		}
+		return soma / vetor.length;
 	}
 	
 	public static void main(String[] args) {
@@ -24,7 +30,9 @@ public class ExMet014 {
 		System.out.println("Digite a sua terceira nota:");
 		double nota3 = input.nextDouble();
 		
-		System.out.println("A sua média é: " + mediaAritmetica(nota1, nota2, nota3));
+		double vetor[] = {nota1, nota2, nota3};
+		
+		System.out.println("A sua média é: " + mediaAritmetica(vetor));
 		
 		input.close();
 	}
